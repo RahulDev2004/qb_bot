9c1e90716fb11f7e6f888fa8ad12b868f64b5ae7
RahulDev2004 <talktorahul.b@gmail.com>
2024-04-19 19:32:18 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import OpenAI from 'openai';
+import Anthropic from '@anthropic-ai/sdk';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const openai = new OpenAI({
-    apiKey: "",
+const anthropic = new Anthropic({
+    apiKey: "", // defaults to process.env["ANTHROPIC_API_KEY"]
 });
 
 
+// const model = "claude-2.1";
+// const url = "https://api.anthropic.com/messages/"
 
 const app = express();
 const port = 3000;
@@ -16,23 +18,50 @@ const port = 3000;
     app.use(cors());
 
     app.post("/", async (req, res) => {
+        try {
 
     const { message } = req.body;
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+    const createQuestionPaper = async (difficulty) => {
+
+    const msg = await anthropic.messages.create({
+        model: "claude-3-opus-20240229",
+        max_tokens: 1000,
+        temperature: 0,
         messages: [
-            {role: "system", content: "You are a assissant who make question paper for the given syllabus. The question paper should have four - 2 mark questions and four - 13 mark questions. Dont add extra content other than question paper."},
-            {role: "user", content: `${message}`},
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": `Create a ${difficulty} difficulty level Question Paper with 5 X 2 mark questions on the syllabus of ${message} and also provide the answer key at the end of the paper.`
+              }
+            ]
+          }
         ]
-    })
+      });
+        console.log(msg.content[0].text);
+        return msg.content[0].text;
+    }
+
+    const easy = await createQuestionPaper("easy");
+    
+    const medium = "hello"
+    const hard = "hello"
 
 res.json({
-    message: completion.choices[0].message.content
+    message: {
+        easy,
+        medium,
+        hard
+    }
 })
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+}
 });
 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
